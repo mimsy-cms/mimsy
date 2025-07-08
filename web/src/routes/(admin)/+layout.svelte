71a8c168720f5c1bbd0bc41f7cee36c62fdc52e0
Alexandre Philibert <alexandre.philibert@ik.me>
2025-07-08 10:48:27 +0200

    Create Sidebar component

---
+++ web/src/routes/(admin)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Sidebar from '$lib/components/admin/Sidebar.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="min-h-screen">
+	<Sidebar />
+
+	<main class="py-6">
+		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			{@render children()}
+		</div>
+	</main>
+</div>
