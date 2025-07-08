d9e88d6218047a3717343e392e436d704732b60b
Alexandre Philibert <alexandre.philibert@ik.me>
2025-07-08 15:15:15 +0200

    Setup http server

--- api/main.go
+++ api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
+	"net/http"
 
 	"github.com/mimsy-cms/mimsy/internal/migrations"
 )
@@ -17,7 +19,26 @@ func main() {
 	migrationCount, err := migrations.Run(context.Background(), runConfig)
 	if err != nil {
 		fmt.Println("Failed to run migrations:", err)
+	} else {
+		fmt.Printf("Successfully ran %d migrations\n", migrationCount)
 	}
 
-	fmt.Printf("Successfully applied %d migrations\n", migrationCount)
+	mux := http.NewServeMux()
+	v1 := http.NewServeMux()
+
+	mux.Handle("/v1/", http.StripPrefix("/v1", v1))
+
+	v1.HandleFunc("POST /auth/login", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	server := &http.Server{
+		Addr:    net.JoinHostPort("localhost", "3000"),
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Println("Failed to start server:", err)
+		return
+	}
 }
