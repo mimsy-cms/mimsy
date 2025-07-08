5bace85f892e64865df0fadc1cc9b310513887d1
Alexandre Philibert <alexandre.philibert@ik.me>
2025-07-08 15:01:05 +0200

    Setup migrations

--- api/main.go
+++ api/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/mimsy-cms/mimsy/internal/migrations"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	runConfig := migrations.NewRunConfig(
+		migrations.WithMigrationsDir("./migrations"),
+		migrations.WithPgURL("postgres://mimsy:mimsy@localhost?sslmode=disable"),
+	)
+
+	// NOTE: Migrations should not be run like this in production.
+	migrationCount, err := migrations.Run(context.Background(), runConfig)
+	if err != nil {
+		fmt.Println("Failed to run migrations:", err)
+	}
+
+	fmt.Printf("Successfully applied %d migrations\n", migrationCount)
 }
