275cd01f8ec83441e44ea75f78f8934d0e7f4856
Red <me@valentin.red>
2025-07-22 11:33:49 +0200

    feat: Bootstraped cli (#82)

---
+++ cli/vitest.config.ts
@@ -0,0 +1,32 @@
+import swc from "unplugin-swc";
+import { loadEnv } from "vite";
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    root: "./",
+    globals: true,
+    isolate: false,
+    passWithNoTests: true,
+    include: ["tests/**/*.test.ts"],
+    env: loadEnv("test", process.cwd(), ""),
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "coverage/unit",
+      include: ["src/**/*.ts"],
+    },
+    typecheck: {
+      enabled: true,
+      tsconfig: "./tsconfig.json",
+    },
+  },
+  resolve: {
+    alias: {
+      $src: path.resolve(__dirname, "./src"),
+      $test: path.resolve(__dirname, "./test"),
+    },
+  },
+  plugins: [swc.vite({ module: { type: "es6" } })],
+});
