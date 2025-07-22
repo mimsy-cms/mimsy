9ccfb2c307ce0cd66842825a1ca9cfc95d1b8deb
Red <me@valentin.red>
2025-07-22 11:34:29 +0200

    feat: Initial version of sdk collection definition (#80)

---
+++ sdk/src/builtins.ts
@@ -0,0 +1,28 @@
+// To make declaration of fields out of this file more difficult.
+const builtInType: unique symbol = Symbol();
+
+export type BuiltInValue = {
+  _marker: typeof builtInType;
+};
+
+export type BuiltIn<T extends BuiltInValue> = {
+  name: string;
+};
+
+export type UserValue = BuiltInValue & {
+  id: string;
+  email: string;
+  name: string;
+};
+
+export type MediaValue = BuiltInValue & {
+  id: string;
+  url: string;
+};
+
+export const User: BuiltIn<UserValue> = {
+  name: "User",
+};
+export const Media: BuiltIn<MediaValue> = {
+  name: "Media",
+};
