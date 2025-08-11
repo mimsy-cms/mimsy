ad6ad0e916a333dfa0515af268b373ed0fbf6a60
Alexandre Philibert <39675639+AlexandrePhilibert@users.noreply.github.com>
2025-08-11 16:04:38 +0200

    Search collections (#165)
    * wip: add search on collections endpoints
    
    * wip: search using search bars

--- api/internal/collection/handler.go
+++ api/internal/collection/handler.go
@@ -18,7 +18,7 @@ func NewHandler(service Service) *Handler {
 func (h *Handler) Definition(w http.ResponseWriter, r *http.Request) {
 	slug := r.PathValue("slug")
 
-	collection, err := h.Service.FindById(r.Context(), slug)
+	collection, err := h.Service.FindBySlug(r.Context(), slug)
 	if err != nil {
 		slog.Error("Failed to get collection definition", "slug", slug, "error", err)
 		if err == ErrNotFound {
@@ -35,7 +35,7 @@ func (h *Handler) Definition(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) GetResources(w http.ResponseWriter, r *http.Request) {
 	slug := r.PathValue("slug")
 
-	collection, err := h.Service.FindById(r.Context(), slug)
+	collection, err := h.Service.FindBySlug(r.Context(), slug)
 	if err != nil {
 		if err == ErrNotFound {
 			http.Error(w, "Collection not found", http.StatusNotFound)
@@ -63,7 +63,7 @@ func (h *Handler) GetResource(w http.ResponseWriter, r *http.Request) {
 	slug := r.PathValue("slug")
 	resourceSlug := r.PathValue("resourceSlug")
 
-	collection, err := h.Service.FindById(r.Context(), slug)
+	collection, err := h.Service.FindBySlug(r.Context(), slug)
 	if err != nil {
 		slog.Error("Failed to get collection", "slug", slug, "error", err)
 		if err == ErrNotFound {
@@ -88,8 +88,19 @@ func (h *Handler) GetResource(w http.ResponseWriter, r *http.Request) {
 	util.JSON(w, http.StatusOK, resource)
 }
 
-func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
-	collections, err := h.Service.FindAll(r.Context())
+type FindAllQueryString struct {
+	Search string `query:"q"`
+}
+
+func (h *Handler) FindAll(w http.ResponseWriter, r *http.Request) {
+	query, err := util.QueryString[FindAllQueryString](r)
+	if err != nil {
+		slog.Error("Failed to decode query parameters", "error", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	collections, err := h.Service.FindAll(r.Context(), &FindAllParams{Search: query.Search})
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
@@ -103,8 +114,19 @@ func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
 	util.JSON(w, http.StatusOK, response)
 }
 
-func (h *Handler) ListGlobals(w http.ResponseWriter, r *http.Request) {
-	globals, err := h.Service.ListGlobals(r.Context())
+type FindAllGlobalsQueryString struct {
+	Search string `query:"q"`
+}
+
+func (h *Handler) FindAllGlobals(w http.ResponseWriter, r *http.Request) {
+	query, err := util.QueryString[FindAllQueryString](r)
+	if err != nil {
+		slog.Error("Failed to decode query parameters", "error", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	globals, err := h.Service.FindAllGlobals(r.Context(), &FindAllParams{Search: query.Search})
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
