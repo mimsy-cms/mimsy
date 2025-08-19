0711445d133717aadb5120963ac286403fd075fc
calum <calum@quinns.ch>
2025-08-19 21:04:31 +0200

    fix: Fix broken merge

--- web/src/routes/(admin)/collections/[collectionSlug]/+page.svelte
+++ web/src/routes/(admin)/collections/[collectionSlug]/+page.svelte
@@ -3,6 +3,20 @@
 	import MoreVerticalIcon from '@lucide/svelte/icons/more-vertical';
 
 	let { data } = $props();
+
+	function formatDate(dateStr: string) {
+		const date = new Date(dateStr);
+		const pad = (n: number) => n.toString().padStart(2, '0');
+
+		const day = pad(date.getDate());
+		const month = pad(date.getMonth() + 1);
+		const year = date.getFullYear();
+		const hours = pad(date.getHours());
+		const minutes = pad(date.getMinutes());
+		const seconds = pad(date.getSeconds());
+
+		return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+	}
 </script>
 
 <div class="flex flex-col gap-6">
@@ -16,26 +30,16 @@
 		<table class="w-full table-fixed divide-y divide-gray-200">
 			<thead class="bg-gray-50">
 				<tr>
-					<th
-						scope="col"
-						class="px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase"
-					>
+					<th class="w-1/4 px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
 						ID
 					</th>
-					<th
-						scope="col"
-						class="px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase"
-					>
+					<th class="w-1/4 px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
 						Slug
 					</th>
-					<th
-						class="w-1/4 px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase"
-					>
+					<th class="w-1/4 px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
 						Created By
 					</th>
-					<th
-						class="w-1/4 px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase"
-					>
+					<th class="w-1/4 px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
 						Last updated
 					</th>
 				</tr>
@@ -57,11 +61,16 @@
 								{resource.slug}
 							</a>
 						</td>
-						<td class="px-6 py-3 text-right text-sm font-medium whitespace-nowrap">
-							<div class="flex items-center justify-end space-x-2">
-								<button class="text-gray-400 hover:text-gray-600">
-									<MoreVerticalIcon class="size-4" />
-								</button>
+
+						<td class="whitespace-nowrap px-6 py-3">
+							<div class="flex items-center text-sm text-gray-500">
+								{resource.created_by_email}
+							</div>
+						</td>
+
+						<td class="whitespace-nowrap px-6 py-3">
+							<div class="flex items-center text-sm text-gray-500">
+								{formatDate(resource.updated_at)}
 							</div>
 						</td>
 					</tr>
