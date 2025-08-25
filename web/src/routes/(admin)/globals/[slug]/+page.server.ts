a768b9c75cd1a40d22aea9f8730aa807e1a66022
Alexandre Philibert <alexandre.philibert@ik.me>
2025-08-25 14:46:26 +0200

    wip: use server actions to submit form

--- web/src/routes/(admin)/globals/[slug]/+page.server.ts
+++ web/src/routes/(admin)/globals/[slug]/+page.server.ts
@@ -1,8 +1,7 @@
-import type { PageServerLoad, Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { env } from '$env/dynamic/public';
-import type { CollectionDefinition } from '$lib/collection/definition';
-import { fail, redirect } from '@sveltejs/kit';
-import { superValidate } from 'sveltekit-superforms';
+import type { CollectionDefinition, CollectionResource } from '$lib/collection/definition';
+import { fail, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import z from 'zod';
 
@@ -14,29 +13,42 @@ async function fetchCollectionDefinition(
 	return response.json();
 }
 
+async function fetchResource(
+	collectionSlug: string,
+	fetch: typeof globalThis.fetch
+): Promise<CollectionResource> {
+	const response = await fetch(`${env.PUBLIC_API_URL}/v1/globals/${collectionSlug}`);
+	return response.json();
+}
+
+const updateSchema = z.record(z.string(), z.any());
+
 export const load: PageServerLoad = async ({ params, fetch }) => {
-	const form = await superValidate(zod(createSchema));
-	const definition = await fetchCollectionDefinition(params.collectionSlug, fetch);
+	const [definition, resource] = await Promise.all([
+		fetchCollectionDefinition(params.slug, fetch),
+		fetchResource(params.slug, fetch)
+	]);
+
+	const form = await superValidate(zod(updateSchema), {
+		defaults: resource
+	});
 
 	return {
-		slug: params.collectionSlug,
+		slug: params.slug,
 		form,
-		definition
+		definition,
+		resource
 	};
 };
 
-const createSchema = z.record(z.string(), z.any());
-
 export const actions: Actions = {
 	default: async ({ request, params, fetch }) => {
-		const collectionSlug = params.collectionSlug;
-
-		const form = await superValidate(request, zod(createSchema), { strict: false });
+		const form = await superValidate(request, zod(updateSchema));
 		if (!form.valid) {
 			return fail(400, { form });
 		}
 
-		const response = await fetch(`${env.PUBLIC_API_URL}/v1/collections/${collectionSlug}`, {
+		const response = await fetch(`${env.PUBLIC_API_URL}/v1/collections/${params.slug}`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
@@ -55,12 +67,8 @@ export const actions: Actions = {
 			} else if (response.status === 404) {
 				return fail(404, { error: 'Collection not found' });
 			} else {
-				return fail(response.status, { error: `Failed to create resource: ${errorText}` });
+				return fail(response.status, { error: `Failed to update resource: ${errorText}` });
 			}
 		}
-
-		const createdResource = await response.json();
-
-		throw redirect(303, `/collections/${collectionSlug}/${createdResource.slug}`);
 	}
 };
---
+++ web/src/routes/(admin)/globals/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ResourceForm from '$lib/components/admin/ResourceForm.svelte';
+	import { superForm } from 'sveltekit-superforms';
+
+	const { data } = $props();
+
+	const { form, message, enhance } = superForm(data.form);
+</script>
+
+<form method="POST" use:enhance>
+	<ResourceForm {form} definition={data.definition} slugEditable={false} />
+</form>
