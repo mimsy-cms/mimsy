89060e6b5f67e3a95f5c06d75d66aaa5e71a9e13
Alexandre Philibert <39675639+AlexandrePhilibert@users.noreply.github.com>
2025-08-26 14:14:00 +0200

    feat: refactor auth user hook (#321)

--- web/src/routes/(admin)/+layout.server.ts
+++ web/src/routes/(admin)/+layout.server.ts
@@ -1,18 +1,12 @@
-import { redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
 import type { LayoutServerLoad } from './$types';
 import type { Collection } from '$lib/collection/definition';
 
-export const load: LayoutServerLoad = async ({ fetch, locals }) => {
-	if (!locals.user) {
-		throw redirect(303, '/login');
-	}
-
-	const response = await fetch(`${env.PUBLIC_API_URL}/v1/collections`);
-	const globalResponse = await fetch(`${env.PUBLIC_API_URL}/v1/collections/globals`);
-
-	const collections = (await response.json()) as Collection[];
-	const globals = (await globalResponse.json()) as Collection[];
+export const load: LayoutServerLoad = async ({ fetch }) => {
+	const [collections, globals]: [Collection[], Collection[]] = await Promise.all([
+		fetch(`${env.PUBLIC_API_URL}/v1/collections`).then((res) => res.json()),
+		fetch(`${env.PUBLIC_API_URL}/v1/collections/globals`).then((res) => res.json())
+	]);
 
 	return {
 		collections,
