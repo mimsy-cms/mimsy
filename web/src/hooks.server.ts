842a527a5f9b94c92281c3699e073fea81b61462
Alexandre Philibert <alexandre.philibert@ik.me>
2025-08-26 13:42:11 +0200

    feat: refactor sveltekit auth check

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,23 +1,14 @@
-import { redirect, type Handle } from '@sveltejs/kit';
+import { type Handle } from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const token = event.cookies.get('session');
 
 	if (token) {
-		const res = await fetch(`${env.PUBLIC_API_URL}/v1/auth/me`, {
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		});
+		const res = await event.fetch(`${env.PUBLIC_API_URL}/v1/auth/me`);
 
 		if (res.ok) {
 			const user = await res.json();
-
-			if (user.must_change_password && event.url.pathname !== '/password') {
-				redirect(303, '/password');
-			}
-
 			event.locals.user = user;
 		}
 	}
